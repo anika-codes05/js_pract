92330f61d40c1f12bc849dcfb43101e5a02c7ff1
anikatyagi05 <anika.25161029@kiet.edu>
2026-07-28 00:21:17 +0530

    commit 1
    basic

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('god bless you all ');
+let a=20
+console.log(a)
+//variable can be defiened by two key words. ->let and var
+//constant ->value can not be changed and it is done by keyword-> const
+const name='vidhur'
+
+//name='vivan'// shows error if assigned the value again because it is constant
+//dynamic typing -> variable can be assigned any type of value and it can be changed at any time
+let b='kanika'
+console.log(b)
+b=3
+console.log(b)
